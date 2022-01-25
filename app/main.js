2422dc835af16d05b44e4216a169fdde431e82a0
awaisanwar544 <awaisanwar544@gmail.com>
2022-01-26 04:53:56 +0500

    Changes for linter errors

--- app/main.js
+++ app/main.js
@@ -18,82 +18,82 @@ navItems.forEach((item) => {
 });
 
 // code for modal
- 
+
 const hideModal = () => {
   document.querySelector('#modal-wrapper').style.display = 'none';
 };
- 
+
 const projectBtn = document.querySelectorAll('.project-btn');
 projectBtn.forEach((item) => {
   item.addEventListener('click', displayModal);
 });
- 
+
 const cancelBtnModal = document.querySelector('#modal-cancel');
 cancelBtnModal.addEventListener('click', hideModal);
- 
+
 // data for projects
- 
-let projects= {
+
+const projects = {
   0: {
-    image: "./assets/project-image1.png",
-    title: "Research Roof - A wordpress project",
-    tech: ["HTML", "CSS", "Javascript", "Ruby on Rails"],
-    description: "orem ipsum dolor sit amet consectetur adipisicing elit. Dolore distinctio aut illum officia unde, quod aliquam eos delectus aspernatur temporibus accusantium, sint quisquam sed quaerat provident tempora reprehenderit explicabo amet.",
-    liveLink: "https://github.com/awaisanwar544/portfolio",
-    gitLink: "https://github.com/awaisanwar544/portfolio"
+    image: './assets/project-image1.png',
+    title: 'Research Roof - A wordpress project',
+    tech: ['HTML', 'CSS', 'Javascript', 'Ruby on Rails'],
+    description: 'orem ipsum dolor sit amet consectetur adipisicing elit. Dolore distinctio aut illum officia unde, quod aliquam eos delectus aspernatur temporibus accusantium, sint quisquam sed quaerat provident tempora reprehenderit explicabo amet.',
+    liveLink: 'https://github.com/awaisanwar544/portfolio',
+    gitLink: 'https://github.com/awaisanwar544/portfolio',
   },
   1: {
-    image: "./assets/images.jpeg",
+    image: './assets/images.jpeg',
     title: "Project 2: I'm working on it",
-    tech: ["HTML", "CSS", "Javascript", "Ruby on Rails"],
-    description: "orem ipsum dolor sit amet consectetur adipisicing elit. Dolore distinctio aut illum officia unde, quod aliquam eos delectus aspernatur temporibus accusantium, sint quisquam sed quaerat provident tempora reprehenderit explicabo amet.",
-    liveLink: "https://github.com/awaisanwar544/portfolio",
-    gitLink: "https://github.com/awaisanwar544/portfolio"
+    tech: ['HTML', 'CSS', 'Javascript', 'Ruby on Rails'],
+    description: 'orem ipsum dolor sit amet consectetur adipisicing elit. Dolore distinctio aut illum officia unde, quod aliquam eos delectus aspernatur temporibus accusantium, sint quisquam sed quaerat provident tempora reprehenderit explicabo amet.',
+    liveLink: 'https://github.com/awaisanwar544/portfolio',
+    gitLink: 'https://github.com/awaisanwar544/portfolio',
   },
   2: {
-    image: "./assets/project-image1.png",
+    image: './assets/project-image1.png',
     title: "Project 3: I'm working on it",
-    tech: ["HTML", "CSS", "Javascript", "Ruby on Rails"],
-    description: "orem ipsum dolor sit amet consectetur adipisicing elit. Dolore distinctio aut illum officia unde, quod aliquam eos delectus aspernatur temporibus accusantium, sint quisquam sed quaerat provident tempora reprehenderit explicabo amet.",
-    liveLink: "https://github.com/awaisanwar544/portfolio",
-    gitLink: "https://github.com/awaisanwar544/portfolio"
+    tech: ['HTML', 'CSS', 'Javascript', 'Ruby on Rails'],
+    description: 'orem ipsum dolor sit amet consectetur adipisicing elit. Dolore distinctio aut illum officia unde, quod aliquam eos delectus aspernatur temporibus accusantium, sint quisquam sed quaerat provident tempora reprehenderit explicabo amet.',
+    liveLink: 'https://github.com/awaisanwar544/portfolio',
+    gitLink: 'https://github.com/awaisanwar544/portfolio',
   },
   3: {
-    image: "./assets/images.jpeg",
+    image: './assets/images.jpeg',
     title: "Project 4: I'm working on it",
-    tech: ["HTML", "CSS", "Javascript", "Ruby on Rails"],
-    description: "orem ipsum dolor sit amet consectetur adipisicing elit. Dolore distinctio aut illum officia unde, quod aliquam eos delectus aspernatur temporibus accusantium, sint quisquam sed quaerat provident tempora reprehenderit explicabo amet.",
-    liveLink: "https://github.com/awaisanwar544/portfolio",
-    gitLink: "https://github.com/awaisanwar544/portfolio"
+    tech: ['HTML', 'CSS', 'Javascript', 'Ruby on Rails'],
+    description: 'orem ipsum dolor sit amet consectetur adipisicing elit. Dolore distinctio aut illum officia unde, quod aliquam eos delectus aspernatur temporibus accusantium, sint quisquam sed quaerat provident tempora reprehenderit explicabo amet.',
+    liveLink: 'https://github.com/awaisanwar544/portfolio',
+    gitLink: 'https://github.com/awaisanwar544/portfolio',
   },
   4: {
-    image: "./assets/images.jpeg",
+    image: './assets/images.jpeg',
     title: "Project 5: I'm working on it",
-    tech: ["HTML", "CSS", "Javascript", "Ruby on Rails"],
-    description: "orem ipsum dolor sit amet consectetur adipisicing elit. Dolore distinctio aut illum officia unde, quod aliquam eos delectus aspernatur temporibus accusantium, sint quisquam sed quaerat provident tempora reprehenderit explicabo amet.",
-    liveLink: "https://github.com/awaisanwar544/portfolio",
-    gitLink: "https://github.com/awaisanwar544/portfolio"
+    tech: ['HTML', 'CSS', 'Javascript', 'Ruby on Rails'],
+    description: 'orem ipsum dolor sit amet consectetur adipisicing elit. Dolore distinctio aut illum officia unde, quod aliquam eos delectus aspernatur temporibus accusantium, sint quisquam sed quaerat provident tempora reprehenderit explicabo amet.',
+    liveLink: 'https://github.com/awaisanwar544/portfolio',
+    gitLink: 'https://github.com/awaisanwar544/portfolio',
   },
   5: {
-    image: "./assets/images.jpeg",
+    image: './assets/images.jpeg',
     title: "Project 6: I'm working on",
-    tech: ["HTML", "CSS", "Javascript", "Ruby on Rails"],
-    description: "orem ipsum dolor sit amet consectetur adipisicing elit. Dolore distinctio aut illum officia unde, quod aliquam eos delectus aspernatur temporibus accusantium, sint quisquam sed quaerat provident tempora reprehenderit explicabo amet.",
-    liveLink: "https://github.com/awaisanwar544/portfolio",
-    gitLink: "https://google.com"
-  }
+    tech: ['HTML', 'CSS', 'Javascript', 'Ruby on Rails'],
+    description: 'orem ipsum dolor sit amet consectetur adipisicing elit. Dolore distinctio aut illum officia unde, quod aliquam eos delectus aspernatur temporibus accusantium, sint quisquam sed quaerat provident tempora reprehenderit explicabo amet.',
+    liveLink: 'https://github.com/awaisanwar544/portfolio',
+    gitLink: 'https://google.com',
+  },
 };
- 
+
 // populate data on existing projects
- 
+
 function addData() {
-  const projectsList = document.querySelectorAll('.work-item')
-  
+  const projectsList = document.querySelectorAll('.work-item');
+
   for (const item in projectsList) {
-    projectsList[item].querySelector('img').src = projects[item]['image'];
-    projectsList[item].querySelector('h2').innerHTML = projects[item]['title'];
+    projectsList[item].querySelector('img').src = projects[item].image;
+    projectsList[item].querySelector('h2').innerHTML = projects[item].title;
     projectsList[item].querySelectorAll('.lang-item').forEach((value, index)=> {
-      value.firstChild.innerHTML = projects[item]['tech'][index]
+      value.firstChild.innerHTML = projects[item].tech[index]
     });
   };
 };
@@ -107,31 +107,29 @@ function displayModal() {
 
 function addDataToModal(ref){
   let btnId = ref.replace(/[^0-9]/g, '');
-  let img = projects[btnId]['image'];
-  let title = projects[btnId]['title'];
-  let techList = projects[btnId]['tech'];
-  let description = projects[btnId]['description'];
-  let liveLink = projects[btnId]['liveLink'];
-  let gitLink = projects[btnId]['gitLink'];
+  let img = projects[btnId].image;
+  let title = projects[btnId].title;
+  let techList = projects[btnId].tech;
+  let description = projects[btnId].description;
+  let liveLink = projects[btnId].liveLink;
+  let gitLink = projects[btnId].gitLink;
 
 
   //add techlist
   let modalList = document.querySelector('#modal-list');
-  modalList.innerHTML = "";
+  modalList.innerHTML = '';
 
   techList.forEach((value) => {
-    let node = document.createElement('li');
-    node.className = "lang-item";
-    let textNode = document.createTextNode(value);
+    const node = document.createElement('li');
+    node.className = 'lang-item';
+    const textNode = document.createTextNode(value);
     node.appendChild(textNode);
     modalList.appendChild(node);
   });
-
 
   document.querySelector('#screenshot').src = img;
   document.querySelector('#modal-title').innerHTML = title;
   document.querySelector('#description').innerHTML = description;
   document.querySelector('#live-link').href = liveLink;
   document.querySelector('#git-link').href = gitLink;
-
-};
+}
